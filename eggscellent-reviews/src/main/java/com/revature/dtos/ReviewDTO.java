1329e353c9bccddbf8a7e78d42875390e54821f5
Ian Nelson <ibnelson24@gmail.com>
2022-11-12 14:20:44 -0500

    review dto change

--- eggscellent-reviews/src/main/java/com/revature/dtos/ReviewDTO.java
+++ eggscellent-reviews/src/main/java/com/revature/dtos/ReviewDTO.java
@@ -12,23 +12,20 @@ import java.util.Objects;
 
 public class ReviewDTO{
 
+    //editing review DTO on 11/12 to no longer include UserDTO and Movie obj
     private String id;
 
-    private UserDTO userId;
+
     private String title;
     private String summary;
 
-    private Movie movieId;
+
 
     public ReviewDTO(Review review) {
         this.id = review.getId();
-        this.userId = new UserDTO(review.getUserId());
         this.title = review.getTitle();
         this.summary = review.getSummary();
-        this.movieId = review.getMovieId();
-    }
 
-    public ReviewDTO() {
     }
 
     public String getId() {
@@ -37,14 +34,6 @@ public class ReviewDTO{
 
     public void setId(String id) {
         this.id = id;
-    }
-
-    public UserDTO getUserId() {
-        return userId;
-    }
-
-    public void setUserId(UserDTO userId) {
-        this.userId = userId;
     }
 
     public String getTitle() {
@@ -63,35 +52,24 @@ public class ReviewDTO{
         this.summary = summary;
     }
 
-    public Movie getMovieId() {
-        return movieId;
-    }
-
-    public void setMovieId(Movie movieId) {
-        this.movieId = movieId;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ReviewDTO reviewDTO = (ReviewDTO) o;
-        return Objects.equals(id, reviewDTO.id) && Objects.equals(userId, reviewDTO.userId) && Objects.equals(title, reviewDTO.title) && Objects.equals(summary, reviewDTO.summary) && Objects.equals(movieId, reviewDTO.movieId);
+        return Objects.equals(id, reviewDTO.id) && Objects.equals(title, reviewDTO.title) && Objects.equals(summary, reviewDTO.summary);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, userId, title, summary, movieId);
+        return Objects.hash(id, title, summary);
     }
-
     @Override
     public String toString() {
         return "ReviewDTO{" +
                 "id='" + id + '\'' +
-                ", userId=" + userId +
                 ", title='" + title + '\'' +
                 ", summary='" + summary + '\'' +
-                ", movieId=" + movieId +
                 '}';
     }
 }
